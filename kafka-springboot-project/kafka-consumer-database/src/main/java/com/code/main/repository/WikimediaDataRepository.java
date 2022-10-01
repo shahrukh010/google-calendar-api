c64618dc1bd308808e62d5974d34869d4ba3accf
Shahrukh Khan <93510831+shahrukh010@users.noreply.github.com>
2022-10-01 19:06:25 +0530

    Delete kafka-springboot-project directory

--- kafka-springboot-project/kafka-consumer-database/src/main/java/com/code/main/repository/WikimediaDataRepository.java
+++
@@ -1,9 +0,0 @@
-package com.code.main.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.code.main.entity.WikimediaData;
-
-public interface WikimediaDataRepository extends JpaRepository<WikimediaData, Long> {
-
-}
